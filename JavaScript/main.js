c2d23c4b33ecce1e93bbb1d82912b30087c3b172
AHMEDSAYEDMO <medos@DESKTOP-GDFMFG1.com>
2022-09-06 17:49:29 +0200

    first project

---
+++ JavaScript/main.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+    showDiva(slideIndex);
+
+function plusDiva(n) {
+    showDiva(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDiva(slideIndex = n);
+}
+
+function showDiva(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-white";
+}
